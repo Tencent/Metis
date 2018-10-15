b8b3c95698f0a51181825511fa647a7757df5149
dongdonglin(林向东) <dongdonglin@tencent.com>
2018-10-15 17:08:06 +0800

    feat(file) delete file

--- tests/test_class.py
+++
@@ -1,9 +0,0 @@
-class TestClass:
-
-    def test_one(self):
-        x = "this"
-        assert 'h' in x
-
-    def test_two(self):
-        x = "hello"
-        assert 'hello' in x
--- tests/test_eg.py
+++
@@ -1,9 +0,0 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
-
-
-def test_answer():
-    assert func(4) == 5
-
-
