64c267b57ac17111fc55efb95fc8f11e455e32dc
dongdonglin(林向东) <dongdonglin@tencent.com>
2018-10-15 11:36:41 +0800

    feat(ci) update ci

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # Run tests
 
-# echo path
 echo $DIR, `pwd`
-echo "hello world"
 
 py.test -x -vv -s `pwd`/tests/
-sh `pwd`/ci/init_mysql_data.sh
+#sh `pwd`/ci/init_mysql_data.sh
