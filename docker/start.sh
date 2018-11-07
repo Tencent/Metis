86076d843fab582c46728a7916a81aef7f1b78c3
lxd1190 <dongdonglin@tencent.com>
2018-11-07 17:53:44 +0800

    fix(docker): update docker image

--- docker/start.sh
+++ docker/start.sh
@@ -12,5 +12,5 @@ docker rm -f metis-web
 
 docker run --net=host --name=metis-db -d -p 3306:3306 -v /data/metis/mysql/:/var/lib/mysql:Z -e MYSQL_ROOT_PASSWORD=metis@123 zhiyunmetis/metis-db
 sleep 6
-docker run --net=host --name=metis-svr -d -p 8080:8080 -v /data/metis/module/:/metis/app/model/time_series_detector:Z zhiyunmetis/metis-svr /bin/sh /metis/init.sh
+docker run --net=host --name=metis-svr -d -p 8080:8080 -v /data/metis/model/:/metis/time_series_detector/model:Z zhiyunmetis/metis-svr /bin/sh /metis/init.sh
 docker run --net=host --name=metis-web -d -p 80:80 zhiyunmetis/metis-web /bin/sh /metis/init.sh ${ip}
