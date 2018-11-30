a8498c404cea3231a9c2d1106749602449fc42f8
codingcrush <qiuye.zhu@ele.me>
2018-11-30 11:26:50 +0800

    fix code alignment of wrapper

--- time_series_detector/demo/golang/wrapper.h
+++ time_series_detector/demo/golang/wrapper.h
@@ -37,16 +37,16 @@ float callValuePredict(void * mhandle, int* data_a, int* data_b, int* data_c, in
 	int sample_result=0;
 	float prob=0;
 
-    ValueData *dt=malloc(sizeof(ValueData));
-    dt->data_a = (int*)malloc(sizeof(int)*len_a);
-    dt->data_b = (int*)malloc(sizeof(int)*len_b);
-    dt->data_c = (int*)malloc(sizeof(int)*len_c);
-    memcpy(dt->data_a, data_a, sizeof(int)*len_a);
-    memcpy(dt->data_b, data_b, sizeof(int)*len_b);
-    memcpy(dt->data_c, data_c, sizeof(int)*len_c);
+	ValueData *dt=malloc(sizeof(ValueData));
+	dt->data_a = (int*)malloc(sizeof(int)*len_a);
+	dt->data_b = (int*)malloc(sizeof(int)*len_b);
+	dt->data_c = (int*)malloc(sizeof(int)*len_c);
+	memcpy(dt->data_a, data_a, sizeof(int)*len_a);
+	memcpy(dt->data_b, data_b, sizeof(int)*len_b);
+	memcpy(dt->data_c, data_c, sizeof(int)*len_c);
 	dt->len_a = len_a;
-    dt->len_b = len_b;
-    dt->len_c = len_c;
+	dt->len_b = len_b;
+	dt->len_c = len_c;
 
 	value_predict(mhandle, dt, &sample_result,&prob);
 	free(dt->data_a);
@@ -66,15 +66,15 @@ float callRatePredict(double *data_a, double* data_b, double* data_c, int len_a,
 
 	RateData *dt= malloc(sizeof(RateData));
 
-    dt->data_a = (double*)malloc(sizeof(double)*len_a);
-    dt->data_b = (double*)malloc(sizeof(double)*len_b);
-    dt->data_c = (double*)malloc(sizeof(double)*len_c);
-    memcpy(dt->data_a, data_a, sizeof(double)*len_a);
-    memcpy(dt->data_b, data_b, sizeof(double)*len_b);
-    memcpy(dt->data_c, data_c, sizeof(double)*len_c);
+	dt->data_a = (double*)malloc(sizeof(double)*len_a);
+	dt->data_b = (double*)malloc(sizeof(double)*len_b);
+	dt->data_c = (double*)malloc(sizeof(double)*len_c);
+	memcpy(dt->data_a, data_a, sizeof(double)*len_a);
+	memcpy(dt->data_b, data_b, sizeof(double)*len_b);
+	memcpy(dt->data_c, data_c, sizeof(double)*len_c);
 	dt->len_a = len_a;
-    dt->len_b = len_b;
-    dt->len_c = len_c;
+	dt->len_b = len_b;
+	dt->len_c = len_c;
 
 	rate_predict(dt, &sample_result, &prob);
 	free(dt->data_a);
